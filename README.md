208523eaddabfd9e25d7d5542ab88839175eac8c
zchhhhh <2069820192@qq.com>
2023-07-26 14:12:41 +0800

    feat: 新增property-resolver

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ A framework written in the style of Spring-Framework.
 只需要定义资源类以及相应的资源扫描类即可，可以扫描Class文件、Jar文件、普通文件
 具体代码详见 `resource-resolve` 下的 `Resource` 和 `ResourceResolve`
 测试代码都在 `test` 目录下
+## 注入 property-resolve
+主要作用是扫描properties和yaml文件中的key-value
---
+++ property-resolve/src/test/java/cdu/zch/winter/utils/YamlUtilsTest.java
@@ -0,0 +1,34 @@
+package cdu.zch.winter.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+/**
+ * @author Zch
+ * @date 2023/7/26
+ **/
+public class YamlUtilsTest {
+
+    @Test
+    public void testLoadYaml() {
+        Map<String, Object> configs = YamlUtils.loadYamlAsPlainMap("application.yml");
+        for (String key : configs.keySet()) {
+            Object value = configs.get(key);
+            System.out.println(key + ": " + value + " (" + value.getClass() + ")");
+        }
+        assertEquals("Winter Framework", configs.get("app.title"));
+        assertEquals("1.0.0", configs.get("app.version"));
+        assertNull(configs.get("app.author"));
+
+        assertEquals("${AUTO_COMMIT:false}", configs.get("summer.datasource.auto-commit"));
+        assertEquals("level-4", configs.get("other.deep.deep.level"));
+
+        assertEquals("0x1a2b3c", configs.get("other.hex-data"));
+        assertEquals("0x1a2b3c", configs.get("other.hex-string"));
+    }
+
+}
